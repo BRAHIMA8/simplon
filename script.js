fdac1f3b122fa12f797c7a8dce39f0564c7d42f7
BRAHIMA8 <brahimakone5@gmail.com>
2025-02-09 00:59:52 +0000

    Presentation en miniature

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById('myButton').addEventListener('click', function() {
-  alert('Bouton cliqué!');
+console.log("Le script est bien chargé !");
+
+document.getElementById("presentationButton").addEventListener("click", function() {
+    var messageText = "C'était Brahima KONE. Merci d'avoir suivi ma présentation personnelle en miniature ainsi que mon parcours. J'ai hâte de faire équipe avec vous!";
+    
+    var messageContainer = document.getElementById("messageContainer");
+    // Affichage du message
+    messageContainer.innerHTML = "<p>" + messageText + "</p>";
+    
+    var imgElement = document.createElement("img");
+    
+    imgElement.src = "image/O'brien.jpg"; 
+    imgElement.alt = "Image de Brahima KONE";
+    
+    // Insertion de l'image 
+    messageContainer.appendChild(imgElement);
 });
